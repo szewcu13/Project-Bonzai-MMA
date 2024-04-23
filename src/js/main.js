f45ab6f29912befe368780e01d31ab2251b04e0e
Kamil <kamil.szewczyk.nsz@gmail.com>
2024-04-23 15:16:59 +0200

    Edited section trainings, added new timeplan

--- src/js/main.js
+++ src/js/main.js
@@ -5,16 +5,19 @@ const allNavLinks = document.querySelectorAll('.nav-animation');
 const allSections = document.querySelectorAll('.section');
 const footerYear = document.querySelector('.footer-year');
 
-// const gallery = document.querySelectorAll('.gallery .gallery-box');
-// const previewBox = document.querySelector('.preview-box');
-// const previewImg = previewBox.querySelector('img');
-// const closeBtn = document.querySelector('.icon');
-// const currentImg = previewBox.querySelector('.current-img')
-// const totalImg = previewBox.querySelector('.total-img')
-// const galleryShadow = document.querySelector('.gallery-shadow')
 
-// const panelOptionOne = document.querySelector('.panel__option-one')
-// const newsBtn = document.querySelector('.news__option-btn')
+// GALLERY 
+
+const gallery = document.querySelectorAll('.gallery .gallery-box');
+const previewBox = document.querySelector('.preview-box');
+const previewImg = previewBox.querySelector('img');
+const closeBtn = document.querySelector('.icon');
+const currentImg = previewBox.querySelector('.current-img')
+const totalImg = previewBox.querySelector('.total-img')
+const galleryShadow = document.querySelector('.gallery-shadow')
+
+const panelOptionOne = document.querySelector('.panel__option-one')
+const newsBtn = document.querySelector('.news__option-btn')
 
 // MOBILE NAV
 
@@ -93,66 +96,66 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // GALLERY
 
-// window.onload = () => {
-// 	for (let i = 0; i < gallery.length; i++) {
-// 		totalImg.textContent = gallery.length;
-// 		let newIndex = i;
-// 		let clickImgIndex;
-// 		gallery[i].onclick = () => {
-// 			clickImgIndex = newIndex;
-// 			function preview() {
-// 				currentImg.textContent = newIndex + 1;
-// 				let selectedImgUrl = gallery[newIndex].querySelector('img').src;
-// 				previewImg.src = selectedImgUrl;
-// 			}
+window.onload = () => {
+	for (let i = 0; i < gallery.length; i++) {
+		totalImg.textContent = gallery.length;
+		let newIndex = i;
+		let clickImgIndex;
+		gallery[i].onclick = () => {
+			clickImgIndex = newIndex;
+			function preview() {
+				currentImg.textContent = newIndex + 1;
+				let selectedImgUrl = gallery[newIndex].querySelector('img').src;
+				previewImg.src = selectedImgUrl;
+			}
 
-// 			const prevBtn = document.querySelector('.prev')
-// 			const nextBtn = document.querySelector('.next')
-// 			if(newIndex == 0) {
-// 				prevBtn.style.display = 'none';
-// 			}
+			const prevBtn = document.querySelector('.prev')
+			const nextBtn = document.querySelector('.next')
+			if(newIndex == 0) {
+				prevBtn.style.display = 'none';
+			}
 
-// 			if(newIndex >= gallery.length - 1) {
-// 				nextBtn.style.display = 'none'
-// 			}
+			if(newIndex >= gallery.length - 1) {
+				nextBtn.style.display = 'none'
+			}
 
-// 			prevBtn.onclick = () => {
-// 				newIndex--;
-// 				if(newIndex == 0) {
-// 					preview();
-// 					prevBtn.style.display = 'none'
-// 				} else {
-// 					preview();
-// 					nextBtn.style.display = 'block'
-// 				}
-// 			}
-// 			nextBtn.onclick = () => {
-// 				newIndex++;
-// 				if(newIndex >= gallery.length - 1) {
-// 					preview()
-// 					nextBtn.style.display = 'none'
-// 				} else {
-// 					preview();
-// 					prevBtn.style.display = 'block'
-// 				}
-// 			}
+			prevBtn.onclick = () => {
+				newIndex--;
+				if(newIndex == 0) {
+					preview();
+					prevBtn.style.display = 'none'
+				} else {
+					preview();
+					nextBtn.style.display = 'block'
+				}
+			}
+			nextBtn.onclick = () => {
+				newIndex++;
+				if(newIndex >= gallery.length - 1) {
+					preview()
+					nextBtn.style.display = 'none'
+				} else {
+					preview();
+					prevBtn.style.display = 'block'
+				}
+			}
 
 
 
-// 			preview();
-// 			previewBox.classList.add('show');
-// 			galleryShadow.style.display = 'block';
+			preview();
+			previewBox.classList.add('show');
+			galleryShadow.style.display = 'block';
 
-// 			closeBtn.onclick = () => {
-// 				newIndex = clickImgIndex;
-// 				prevBtn.style.display = 'block'
-// 				nextBtn.style.display = 'block'
-// 				previewBox.classList.remove('show');
-// 				galleryShadow.style.display = 'none';
-// 			};
-// 		};
-// 	}
-// };
+			closeBtn.onclick = () => {
+				newIndex = clickImgIndex;
+				prevBtn.style.display = 'block'
+				nextBtn.style.display = 'block'
+				previewBox.classList.remove('show');
+				galleryShadow.style.display = 'none';
+			};
+		};
+	}
+};
 
 burgerBtn.addEventListener('click', showMobileMenu);
 // newsBtn.addEventListener('click', showNewsPanel);
